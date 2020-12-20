1cca7d3388c5b6a4470277ae1b8cf51810e0d124
jeunwork <74290918+jeunwork@users.noreply.github.com>
2020-12-20 19:58:37 +0900

    HTML,CSS,JS: viewmore toggle

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* navbar toggle */
 const toggleBtn = document.querySelector('.navbar_toogleBtn');
 const menu = document.querySelector('.navbar_menu');
 const icons = document.querySelector('.navbar_icons');
@@ -5,4 +6,12 @@ const icons = document.querySelector('.navbar_icons');
 toggleBtn.addEventListener('click', function () {
         menu.classList.toggle('active');
         icons.classList.toggle('active');
+    });
+
+/* viewmore toggleBtn */
+const toogleBtn = document.querySelector('.view_toggleBtn');
+const viewmore = document.querySelector('.viewmore');
+
+toogleBtn.addEventListener('click', function () {
+        viewmore.classList.toggle('active');
     });
